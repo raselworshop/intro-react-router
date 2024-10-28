802a697ab32db52e1ff1b6066376c55b74020cdd
raselworshop <raselworshop@gmail.com>
2024-10-28 06:11:32 -0400

    added first commit about router

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello react router</div>
+  },
+  {
+    path:'/about',
+    element: <div>I am in about page</div>
+  },
+  {
+    path: '/contacts',
+    element: <div>Call me right now!!</div>
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
